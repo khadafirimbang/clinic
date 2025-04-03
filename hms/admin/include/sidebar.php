d0ae534c82e52244796bc1864bff53a748637b29
Khadafi Rimbang <khadafirimbang7@gmail.com>
2025-04-03 19:25:14 +0800

    first commit

---
+++ hms/admin/include/sidebar.php
@@ -0,0 +1,212 @@
+
+
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+  <ul class="main-navigation-menu">
+		  <li> <a href="dashboard.php">
+							  <div class="item-content">
+							    <div class="item-media"> </div>
+							    <div class="item-inner">
+							      <span class="title"> <em><strong><center>
+							      <span style="color: #FFFFFF">HOME</span></strong></em></span></div>
+						    </div>
+							</a>							 
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+                            <div class="item-content">
+                              <div class="item-media"> </div>
+                              <div class="item-inner">
+                                <span class="title"> <em><strong><center>
+                                <span style="color: #FFFFFF">DOCTORS</span></strong></em></span>
+                              </div>
+                            </div>
+                          </a>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> Doctor Specialization </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> Add Doctor</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> View Doctors </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner"><em><strong><center><span style="color: #FFFFFF">USERS</strong></em></div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> View Users </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner">
+											<span class="title"> <em><strong><center><span style="color: #FFFFFF">PATIENTS</strong></em></span></div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-patient.php">
+											<span class="title"> View Patients </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner">
+											<span class="title"> <strong><em><center><span style="color: #FFFFFF">APPOINTMENT HISTORY</em></strong></span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner">
+											<span class="title"> <strong><em><center><span style="color: #FFFFFF">DIRECT MESSAGES</em></strong></span></div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="unread-queries.php">
+											<span class="title"> Unreads </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="read-query.php">
+											<span class="title"> Read </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+
+
+	<li>
+								<a href="doctor-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner"><em><strong><center><span style="color: #FFFFFF">DOCTOR LOGIN SESSIONS</strong></em></div>
+									</div>
+								</a>
+							</li>		
+
+
+
+							<li>
+								<a href="user-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner">
+											<span class="title"> <strong><em><center><span style="color: #FFFFFF">USER LOGIN SESSIONS</em></strong></span>
+										</div>
+									</div>
+								</a>
+							</li>						
+				<li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner"><strong><em><center><span style="color: #FFFFFF">PAGES SETTINGS</em></strong></div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="about-us.php">
+											<span class="title">About Us </span>
+										</a>
+									</li>
+																	<li>
+										<a href="contact.php">
+											<span class="title">Contact Us </span>
+										</a>
+									</li>
+								</ul>
+							</li>
+
+
+
+
+								<li>
+								<a href="patient-search.php">
+									<div class="item-content">
+										<div class="item-media">
+										</div>
+										<div class="item-inner">
+											<span class="title"> <em><strong><center><span style="color: #FFFFFF">PATIENT SEARCH</strong></em></span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li> <a href="../../queuing/admin.php">
+								<div class="item-content">
+								  <div class="item-media"> </div>
+								  <div class="item-inner">
+								    <span class="title"> <em><strong><center><span style="color: #FFFFFF">QUEUING</strong></em></span>
+						          </div>
+						    </div>
+</li>
+
+
+							<li> <a href="chatbot/admin/login.php">
+								<div class="item-content">
+								  <div class="item-media"> </div>
+								  <div class="item-inner">
+								    <span class="title"> <em><strong><center><span style="color: #FFFFFF">CHATBOT</strong></em></span>
+						          </div>
+						    </div> </li>
+							
+
+						</ul>
+						
+				  </nav>
+  </div>
+</div>
---
+++ hms/doctor/include/sidebar.php
@@ -0,0 +1,95 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar bg-dark">
+
+<nav>
+						
+
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+								<div class="item-content">
+										<div class="item-media">
+										
+										</div>
+										<div class="item-inner">
+											<span class="title"> <em><center>
+											 <span style="color: #FFFFFF"> <strong>HOME</strong>
+											</center></em></span>
+									</div>
+								  </div>
+								</a>
+							</li>
+							
+
+							<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+							
+										</div>
+										<div class="item-inner">
+											<span class="title"><center> 
+											  <span style="color: #FFFFFF"><em><strong>APPOINTMENT HISTORY </strong></em>
+											</center></span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+									<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+		
+										</div>
+										<div class="item-inner">
+										<span class="title">
+											<span style="color: #FFFFFF"><center> <em><strong>PATIENTS </center></strong></em></span>									    <strong></strong></div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<em><a href="add-patient.php">
+											<span class="title">
+												<span style="color: #FFFFFF">Add Patient</span>
+										</a>
+									    </em></li>
+									<li>
+										<em><a href="manage-patient.php">
+											<span class="title">
+												<span style="color: #FFFFFF">Manage Patient </span>
+										</a>
+									    </em></li>
+									
+								</ul>
+								</li>
+<li>
+								        <em><a href="search.php">
+									    <div class="item-content">
+									      <div class="item-media"> </div>
+									      <div class="item-inner"><center>
+									       <span style="color: #FFFFFF"> <strong><em>FIND PATIENTS</em></strong>
+									      </center></div>
+									      </div>
+								        </a></em></li>
+							<li>
+								<a href="https://queuemanagement.lgu2.com/queuing/queue-patient.php">
+									<div class="item-content">
+										<div class="item-media">
+							
+										</div>
+										<div class="item-inner">
+											<span class="title"><center> 
+											  <span style="color: #FFFFFF"><em><strong>QUEUEING </strong></em>
+											</center></span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+						</ul>
+		
+						
+					</nav>
+					</div>
+			</div>
